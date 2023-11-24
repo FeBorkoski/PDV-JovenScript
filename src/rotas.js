0a2b120a35f0ad2c6fb8fd237f3f81d8c778362f
Fernando <fernando.a7x@live.com>
2023-11-24 11:05:00 -0300

    Listar categorias

--- src/rotas.js
+++ src/rotas.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const listarCategorias = require("./controladores/listarCategorias");
+
+
 const rotas = express();
+
+rotas.get("/categoria", listarCategorias);
 
 
 module.exports = rotas;
