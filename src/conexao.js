8466abd29a9b2f5d64eae9521d9a2bb7577cd240
Fernando <fernando.a7x@live.com>
2023-11-24 13:38:03 -0300

    conexao

--- src/conexao.js
+++ src/conexao.js
@@ -4,12 +4,11 @@ require('dotenv').config()
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        port: 5432,
-        user: 'postgres',
-        password: '3430',
-        database: 'pdv'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
     }
 });
 
-module.exports = knex
+module.exports = knex;
