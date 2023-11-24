03c0aa8d3c9011ebe322e72c98ae0929b9884cd1
Fernando <fernando.a7x@live.com>
2023-11-24 00:07:06 -0300

    arrumando as tabelas id auto

--- src/dump.sql
+++ src/dump.sql
@@ -1,16 +1,11 @@
 create database pdv;
 
-create table usuarios (
-    id serial primary key,
-    nome varchar(1000) not null,
-    email varchar(1000) not null unique,
-    senha varchar(1000) not null
-);
 
-create table categorias (
-    id serial primary key,
-    descricao varchar(1000) not null
-);
+create table usuarios ( id serial primary key, nome varchar(1000) not null, email varchar(1000) not null unique, senha varchar(1000) not null);
 
-INSERT INTO "categorias" (id,descricao) VALUES (1,'Informatica'),(2, 'Celulares'),(3, 'Beleza e perfumaria'),(4, 'Mercado');
-INSERT INTO "categorias" (id,descricao) VALUES (5,'Livros e Papelaria'),(6,'Brinquedos'),(7,'Moda'),(8,'Bebê'),(9,'Games');
+
+create table categorias ( id serial primary key, descricao varchar(1000) not null);
+
+
+INSERT INTO categorias (descricao)
+VALUES ('Informática'), ('Celulares'), ('Beleza e Perfumaria'), ('Mercado'), ('Livros e Papelaria'), ('Brinquedos'), ('Moda'), ('Bebê'), ('Games');
