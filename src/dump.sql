fb19cbd3b2a152c832aa2f587d556eba295e476e
Fernando <fernando.a7x@live.com>
2023-11-28 12:04:50 -0300

    SPRINT 1 COMPLETA

--- src/dump.sql
+++ src/dump.sql
@@ -1,4 +1,4 @@
-create database dpdv;
+create database pdv;
 
 
 create table usuarios ( id serial primary key, nome varchar(1000) not null, email varchar(1000) not null unique, senha varchar(1000) not null);
