b8ef5a82ee6d469f98915dde58611fe9f8bdea81
Guilherme Gomes <guilherme09396@gmail.com>
2023-11-28 09:50:10 -0300

    Rota/controlador de atualizar usuário finalizados

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,2 +1,32 @@
-const jwt = require("jsonwebtoken");
+const knex = require("../conexao");
 const bcrypt = require("bcrypt");
+
+const atualizar = async (req, res) => {
+    const { usuario } = req;
+    const { nome, email, senha } = req.body;
+
+    try {
+        const emailExistente = await knex("usuarios").where({ email })
+            .andWhere('id', '!=', usuario.id).first();
+
+        if (emailExistente) return res.status(400).json({ mensagem: "E-mail informado já está cadastrado" })
+
+        const senhaCripto = await bcrypt.hash(senha, 10);
+
+
+        await knex("usuarios").where({ id: usuario.id }).update({
+            nome,
+            email,
+            senha: senhaCripto
+        });
+
+        return res.status(204).json();
+
+    } catch (error) {
+        return res.status(500).json({ mensagem: "Erro interno do servidor" })
+    }
+}
+
+module.exports = {
+    atualizar
+}
--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const listarCategorias = require("./controladores/listarCategorias");
-const { verificarCamposLogin } = require("./intermediarios/validacao");
+const { verificarCamposLogin, validarCampos } = require("./intermediarios/validacao");
 const login = require("./controladores/login");
 const { verificarToken } = require("./intermediarios/autenticacao");
+const { atualizar } = require("./controladores/usuarios");
+const schemaUsuario = require("./schemas/usuario");
 
 const rotas = express();
 
@@ -11,5 +13,6 @@ rotas.get("/categoria", listarCategorias);
 rotas.post("/login", verificarCamposLogin, login);
 
 rotas.use(verificarToken)
+rotas.put("/usuario", validarCampos(schemaUsuario), atualizar);
 
 module.exports = rotas;
