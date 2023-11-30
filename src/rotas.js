a3ffdcd5670d981d35cfa75fe3f57dec7a81106e
Guilherme Gomes <guilherme09396@gmail.com>
2023-11-30 16:38:09 -0300

    Finalizado recurso de cadastrar cliente

--- src/rotas.js
+++ src/rotas.js
@@ -1,33 +1,32 @@
 const express = require("express");
 const listarCategorias = require("./controladores/listarCategorias");
-
 const {
   cadastrar,
   detalharUsuario,
   atualizar,
 } = require("./controladores/usuarios");
-
 const {
   verificarCamposLogin,
   validarCampos,
 } = require("./intermediarios/validacao");
 const login = require("./controladores/login");
 const { verificarToken } = require("./intermediarios/autenticacao");
-
 const schemaUsuario = require("./schemas/usuario");
+const schemaCliente = require("./schemas/cliente");
+
+const { cadastrarCliente } = require("./controladores/cliente");
+
 
 const rotas = express();
 
 rotas.get("/categoria", listarCategorias);
-
 rotas.post("/usuario", validarCampos(schemaUsuario), cadastrar);
-
 rotas.post("/login", verificarCamposLogin, login);
 
 rotas.use(verificarToken);
-
 rotas.get("/usuario", detalharUsuario);
-
 rotas.put("/usuario", validarCampos(schemaUsuario), atualizar);
+
+rotas.post("/cliente", validarCampos(schemaCliente), cadastrarCliente);
 
 module.exports = rotas;
