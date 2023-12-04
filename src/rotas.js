1d856e7758423e24e6aad880987351f23be20f9c
George <georteofilo.dev@gmail.com>
2023-12-04 17:32:35 -0300

    Atualizando o hml

--- src/rotas.js
+++ src/rotas.js
@@ -14,13 +14,12 @@ const {
 const login = require("./controladores/login");
 const { verificarToken } = require("./intermediarios/autenticacao");
 
-const produto = require("./controladores/produtos");
+const { cadastrarProduto, editarProduto } = require("./controladores/produtos");
 
 const schemaUsuario = require("./schemas/usuario");
 const schemaProduto = require("./schemas/schemaProdutos");
 
 const rotas = express();
-
 
 rotas.get("/categoria", listarCategorias);
 
@@ -29,12 +28,12 @@ rotas.post("/usuario", cadastrar)
 rotas.post("/login", verificarCamposLogin, login);
 rotas.use(verificarToken)
 
-rotas.get("/usuario", usuario.detalharUsuario);
+rotas.get("/usuario", detalharUsuario);
 
 rotas.use(verificarToken)
 rotas.put("/usuario", validarCampos(schemaUsuario), atualizar);
-rotas.post("/produto", validarCampos(schemaProduto), produto.cadastrarProduto);
-rotas.put("/produto/:id", validarCampos(schemaProduto), produto.editarProduto);
+rotas.post("/produto", validarCampos(schemaProduto), cadastrarProduto);
+rotas.put("/produto/:id", validarCampos(schemaProduto), editarProduto);
 
 
 module.exports = rotas;
